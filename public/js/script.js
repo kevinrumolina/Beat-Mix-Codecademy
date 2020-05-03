4d2d8a9db3f54a3d843d39bc0babe4d14da13c57
KevinMolina <kevinrumolina26@gmail.com>
2020-05-03 18:51:16 -0500

    script.js functions declaration

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,35 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
+const toogleDrum = (drumName, index) => {
+    if (drumName[index] === false) {
+        drumName[index] = true;
+    } else {
+        drumName[index] = false;
+    }
+
+    return drumName;
+};
+
+const clear = drumName => {
+    const initialArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+    drumName = initialArray;
+    
+    return drumName;
+}
+
+const invert = drumName => {
+    drumName = drumName.map(element => {
+        if (element === false) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    
+    return drumName;
+};
